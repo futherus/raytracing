c830dcaa99115ddaeefbac95d499d074e5bd534a
futherus <simankovich.al@phystech.edu>
2022-09-19 01:56:11 +0300

    Created usage samples.

--- src/axes.h
+++ src/axes.h
@@ -23,7 +23,7 @@ public:
     axes_t(sf::Vector2f  tl_pos, sf::Vector2f  br_pos,
            sf::Vector2f min_lim, sf::Vector2f max_lim)
         : tl_pos_(tl_pos), br_pos_(br_pos), min_lim_(min_lim), max_lim_(max_lim),
-          vertices_(), borders_() //, vec_(), arrow_()
+          vertices_(), borders_()
     {
         borders_.append(sf::Vector2f(tl_pos.x, tl_pos.y));
         borders_.append(sf::Vector2f(tl_pos.x, br_pos.y));
@@ -49,12 +49,8 @@ private:
         
         target.draw(borders_, states);
 
-        sf::VertexArray vec_ = vertices_[1];
-
         for(size_t i = 0; i < vertices_.size(); i++)
-        {
             target.draw(vertices_[i], states);
-        }
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
 #include <SFML/Graphics.hpp>
 #include <unistd.h>
+#include <cmath>
 #include "axes.h"
+
+const float CONST_PI = 3.14159;
 
 int main()
 {
-    vector_t vec(0.5, 0.5);
+    axes_t axes1(sf::Vector2f(100, 100), sf::Vector2f(400, 400),
+                 sf::Vector2f(-1, -1),   sf::Vector2f(1, 1));
 
-    axes_t axes(sf::Vector2f(100, 100), sf::Vector2f(400, 400),
-                sf::Vector2f(-1, -1),   sf::Vector2f(1, 1));
+    axes_t axes2(sf::Vector2f(500, 100), sf::Vector2f(800, 400),
+                 sf::Vector2f(-1, -1),   sf::Vector2f(1, 1));
 
-    axes.draw(vec);
+    axes_t stopwatch_axes(sf::Vector2f(900, 100), sf::Vector2f(1200, 400),
+                          sf::Vector2f(-1, -1),   sf::Vector2f(1, 1));
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window name", sf::Style::Default);
+    sf::Clock stopwatch;
+
+    float angle  = 0;
+    float length = 0.5;
+
+    sf::RenderWindow window(sf::VideoMode(1600, 1200), "My window name", sf::Style::Default);
+    sf::Clock clock;
 
     while(window.isOpen())
     {
@@ -26,33 +37,6 @@ int main()
                     window.close();
                     break;
                 }
-                // case sf::Event::KeyPressed:
-                // {
-                //     switch(event.key.code)
-                //     {
-                //         case sf::Keyboard::Up:
-                //         {
-                //             add_balls(&data, 1);
-                //             break;
-                //         }
-                //         case sf::Keyboard::Down:
-                //         {
-                //             remove_balls(&data, 1);
-                //             break;
-                //         }
-                //         case sf::Keyboard::T:
-                //         {
-                //             printf("T: %f\n", evaluate_temperature(&data));
-                //             break;
-                //         }
-                //         default:
-                //         {
-                //             void(0);
-                //             // fall through
-                //         }
-                //     }
-                //     break;
-                // }
                 default:
                 {
                     // fall through
@@ -61,13 +45,33 @@ int main()
             }
         }
 
-        // sf::Time elapsed = clock.restart();
+        sf::Time elapsed = clock.restart();
+
+        angle += elapsed.asSeconds();
+        vector_t vec (length * std::cos(angle), length * std::sin(angle));
+        vector_t base(length * std::cos(angle * 2), length * std::sin(angle * 2));
+        float secs = CONST_PI / 2 - stopwatch.getElapsedTime().asSeconds() * 2 * CONST_PI / 60;
+        vector_t stopwatch_sec(length * std::cos(secs),
+                               length * std::sin(secs));
+        float mins = CONST_PI / 2 - stopwatch.getElapsedTime().asSeconds() * 2 * CONST_PI / 360;
+        vector_t stopwatch_mins(length * 0.8 * std::cos(mins),
+                                length * 0.8 * std::sin(mins));
 
         window.clear();
+        axes1.clear();
+        axes2.clear();
+        stopwatch_axes.clear();
 
-        axes.clear();
-        axes.draw(vec);
-        window.draw(axes);
+        axes1.draw(vec, base);
+        axes1.draw(base);
+        axes1.draw(base + vec);
+        axes2.draw(vec);
+        stopwatch_axes.draw(stopwatch_sec);
+        stopwatch_axes.draw(stopwatch_mins);
+
+        window.draw(axes1);
+        window.draw(axes2);
+        window.draw(stopwatch_axes);
 
         window.display();
     }
